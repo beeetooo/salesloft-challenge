12f83b7fe8b6b6c71ba9703bbe195b781fdb41d1
Alberto Romero <lguitarras0594@gmail.com>
2020-02-21 13:13:15 -0600

    Finishes draft of sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sidebar />
+    <div class="app__content">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Sidebar,
   }
 }
 </script>
 
 <style>
+html {
+  background-color: #151618;
+}
+
+body {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #A9B6C0;
+  display: flex;
+  padding: 20px 0 20px 0;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="sidebar">
+    <img class="sidebar__logo" :src="logo">
+    <ul class="sidebar__options">
+      <li class="sidebar__option" @click="$emit('showPeople')"> People </li>
+      <li class="sidebar__option" @click="$emit('showCharactersFrequency')"> Frequency </li>
+      <li class="sidebar__option" @click="$emit('showDuplicatedEmails')"> Duplicated </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import Logo from '../assets/logo.png';
+
+export default {
+  name: 'Sidebar',
+  data: function () {
+    return { logo: Logo };
+  }
+}
+</script>
+
+<style lang="scss">
+.sidebar {
+  width: 15%;
+
+  &__logo {
+    width: 80px;
+    height: 80px;
+    align-self: center;
+  }
+
+  &__options {
+    margin-top: 40px;
+    padding: 0 16px 0 16px;
+  }
+
+  &__option {
+    list-style: none;
+    text-align: left;
+    cursor: pointer;
+    margin: 12px;
+    border-radius: 4px;
+    padding: 4px;
+    color: #1997DE;
+    background-color: #2a4554;
+  }
+}
+</style>
