9d90fed4953638ee83c2e0ef1369ada8205652c0
Alberto Romero <lguitarras0594@gmail.com>
2020-02-22 15:18:27 -0600

    Add pagination to people section

---
+++ client/components/Paginator.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="paginator">
+    <button
+      class="paginator__button"
+      :disabled="disablePrevious"
+      @click="$emit('previous')"
+    >
+      Previous
+    </button>
+    <button
+      class="paginator__button"
+      :disabled="disableNext"
+      @click="$emit('next')"
+    >
+      Next
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Paginator',
+
+  props: {
+    disablePrevious: {
+      type: Boolean,
+      required: true
+    },
+    disableNext: {
+      type: Boolean,
+      required: true
+    }
+  },
+}
+</script>
--- client/containers/People.vue
+++ client/containers/People.vue
@@ -1,40 +1,90 @@
 <template>
-  <div class="people">
-    <Person
-      class="people__person"
-      v-for="person in people"
-      :key="person.email"
-      :name="person.name"
-      :title="person.title"
-      :email="person.email"
+  <div class="people-container">
+    <div class="people-container__people">
+      <Person
+        v-for="person in people"
+        :key="person.email"
+        :name="person.name"
+        :title="person.title"
+        :email="person.email"
+
+        class="people__person"
+      />
+    </div>
+
+    <Paginator
+      :disable-previous="disablePrevious"
+      :disable-next="disableNext"
+      @previous="fetchPreviousPage"
+      @next="fetchNextPage"
     />
   </div>
 </template>
 
 <script>
-import Person from '../components/Person.vue'
 import axios from 'axios';
+
+import Paginator from '../components/Paginator.vue'
+import Person from '../components/Person.vue'
 
 export default {
   name: 'People',
 
-  components: { Person },
+  components: {
+    Paginator,
+    Person,
+  },
 
   data: function () {
     return {
       people: [],
+      page: 59,
+      perPage: 6,
     };
   },
 
+  computed: {
+    disablePrevious: function () {
+      return this.page === 1;
+    },
+    disableNext: function () {
+      return this.people.length === 0 || this.people.length < 6;
+    }
+  },
+
   mounted: async function () {
-    const response = await axios.get('http://localhost:5000/people?page=1&per_page=6');
+    const response = await axios.get(
+      `http://localhost:5000/people?page=${this.page}&per_page=${this.perPage}`
+    );
     this.people = response.data.results;
-  }
+  },
+
+  methods: {
+    fetchPreviousPage: async function () {
+      this.page = this.page - 1;
+
+      const response = await this.requestUsers(this.page, this.perPage);
+      this.people = response.data.results;
+    },
+
+    fetchNextPage: async function () {
+      this.page = this.page + 1;
+
+      const response = await this.requestUsers(this.page, this.perPage);
+      this.people = response.data.results;
+    },
+
+    requestUsers: async function (page, perPage) {
+      return await axios.get(
+        `http://localhost:5000/people?page=${page}&per_page=${perPage}`
+      );
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-.people {
+.people-container__people {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
